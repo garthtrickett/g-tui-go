a3f2da5b2aec66bd7f9ebe07d4e8721bcbb564cd
Garth Trickett <garthtrickett@gmail.com>
2025-11-29 12:44:16 +1100

    updated to point to local dev folder

--- default.nix
+++ default.nix
@@ -4,13 +4,16 @@ buildGoModule rec {
   pname = "g-tui-go";
   version = "0.1.0"; #<-- Change this to match your git tags
 
-  src = fetchgit {
-    url = "https://github.com/garthtrickett/g-tui-go";
-    rev = "77801ae14f538693e4e7b6091bfd09cea05f7ab6";
-    sha256 = "sha256-zQhqUgFaYVX+Lcq9SDfQazyxEYUZr/zNuGkj6viOfos=";
-  };
+  # src = fetchgit {
+  #   url = "https://github.com/garthtrickett/g-tui-go";
+  #   rev = "77801ae14f538693e4e7b6091bfd09cea05f7ab6";
+  #   sha256 = "sha256-zQhqUgFaYVX+Lcq9SDfQazyxEYUZr/zNuGkj6viOfos=";
+  # };
 
   vendorHash = "sha256-8A24/OxIub1UUay91PVh+gUsOZJj0pKfR0DOOdrDNPE=";
+
+
+  src = "~/files/code/g-tui-go/";
 
 
 
